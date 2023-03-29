262a71cfac9e87c47002de0e069519c55de71849
Lucasgeg <99218923+Lucasgeg@users.noreply.github.com>
2023-03-30 00:04:40 +0200

    front close to be ok

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,24 +1,83 @@
-import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRef, useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+// Generate a random ID for the chat session
+const randomId = uuidv4();
 
 export const Modal = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const [textInput, setTextInput] = useState();
-  const [messageContent, setMessageContent] = useState([]);
+  // Ref for scrolling to the bottom of the chat window
+  const messagesEndRef = useRef(null);
+
+  // State for the chat modal
+  const [isOpen, setIsOpen] = useState(false);
+
+  // State for the user input text
+  const [textInput, setTextInput] = useState("");
+
+  // State for the chat messages
+  const [messageContent, setMessageContent] = useState([
+    {
+      sender: "lito",
+      content:
+        "Bonjour et bienvenue sur notre Chat intéractif! Vous allez voir on va bien s'entendre!",
+    },
+  ]);
+
+  // Function to handle opening and closing the chat modal
   const handleOpen = () => {
     setIsOpen((prevState) => !prevState);
   };
-  useEffect(() => {});
-  const handleSubmit = (e) => {
+
+  // Function to get a response from the chatbot API
+  const getResponse = async () => {
+    // Use the random ID as the session ID for the chat
+    const sessionId = randomId;
+    const response = await axios
+      .post("http://localhost:8080/dialogflow", {
+        queryText: textInput,
+        sessionId: sessionId,
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return response.data;
+  };
+
+  // UseEffect hook to scroll to the bottom of the chat window when new messages are added
+  useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messageContent]);
+
+  // Function to handle user input submission
+  const handleSubmit = async (e) => {
     if (e.key === "Enter") {
+      // Check for special commands
       if (textInput === "flip") {
         /* animation flip */
       }
-      console.log(textInput);
-      let storedMessage = [
-        "Bonjour et bienvenue sur notre Chat intéractif! Vous allez voir on va bien s'entendre!",
-      ];
-      storedMessage.push(textInput);
-      setMessageContent(storedMessage);
+
+      // Check for maximum message length
+      if (textInput.length >= 255) {
+        return setMessageContent([
+          "Je suis désolé mais votre message est trop long (plus de 256 charactères)",
+        ]);
+      }
+
+      // Get the response from the chatbot API
+      const response = await getResponse();
+
+      // Update the message content with the user input and the chatbot response
+      setMessageContent((prevState) => [
+        ...prevState,
+        { sender: "user", content: textInput },
+      ]);
+      setMessageContent((prevState) => [
+        ...prevState,
+        { sender: "lito", content: response },
+      ]);
+
+      // Clear the user input text
       setTextInput("");
     }
   };
@@ -30,8 +89,11 @@ export const Modal = () => {
         </div>
         <div className="content">
           {messageContent.map((message, index) => (
-            <span key={index}>{message}</span>
+            <span key={index} className={message.sender}>
+              {message.content}
+            </span>
           ))}
+          <div ref={messagesEndRef} />
         </div>
         <div className="chatbox">
           <input
